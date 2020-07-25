6d53c86ac49d60dad9684764194565e91da467c3
Felipe Oliveira <fsousa@tasken.com>
2020-07-24 23:06:18 -0300

    Organização básica da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Cart from './components/CartContainer';
+import DisplayContainer from './components/DisplayContainer';
+import Header from './components/Header';
+import { Container, ApplicationWrapper, Main } from './components/layout';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <ApplicationWrapper>
+      <Header />
+      <Container>
+        <Main>
+          <DisplayContainer />
+        </Main>
+        <aside>
+          <Cart />
+        </aside>
+      </Container>
+    </ApplicationWrapper>
+  );
 };
 
 export default App;
