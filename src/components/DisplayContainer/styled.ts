91f88dc2e057b3994839b5d41b46733edc45f906
Felipe Oliveira <fsousa@tasken.com>
2020-07-26 02:28:00 -0300

    Diminuindo largura do item

--- src/components/DisplayContainer/styled.ts
+++ src/components/DisplayContainer/styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const DisplayWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(192px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(110px, 1fr));
   grid-gap: 20px 16px;
   padding: 20px 16px;
   &.loading {
